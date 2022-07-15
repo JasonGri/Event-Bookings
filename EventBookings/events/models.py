b107e1c85bdf9621a89499e3835b9f63269e1faf
JasonGri <Iason.Grivas@acg.edu>
2022-07-15 15:10:48 +0300

    Added database models

--- EventBookings/events/models.py
+++ EventBookings/events/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Sub_category(models.Model):
+    name = models.CharField(max_length=32)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Location(models.Model):
+    city = models.CharField(max_length=64)
+    country = models.CharField(max_length=32)
+    street = models.CharField(max_length=64)
+    street_num = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.street} {self.street_num}, {self.city}. {self.country}'
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=64)
+    date = models.DateField()
+    duration = models.IntegerField() #In minutes
+    likes = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(Sub_category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'{self.title}, {self.sub_category}.'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dob = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    events = models.ManyToManyField(Event, blank=True, related_name='users')
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Review(models.Model):
+    comment = models.CharField(max_length=256)
+    rating = models.FloatField()
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.comment}, {self.rating}/5 stars.'
